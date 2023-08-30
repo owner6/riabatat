b8d1b3b6c3c93111b8cc65498885d47f4355db85
owner6 <owner1147@gmail.com>
2023-08-30 18:03:38 +0300

    request settings not working

--- backend/handlers/settings-api.js
+++ backend/handlers/settings-api.js
@@ -1,13 +1,13 @@
 const { DynamoDBClient, UpdateItemCommand } = require("@aws-sdk/client-dynamodb");
-const { marshall } = require("@aws-sdk/util-dynamodb");
+const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
 const dynamoDB = new DynamoDBClient({ region: "eu-central-1" });
 
 module.exports.handler = async (event) => {
   try {
+    const userId = event.pathParameters.userId;
     const requestBody = JSON.parse(event.body);
     const { fromYear, toYear, marka, model } = requestBody;
-    const userId = "userId";
 
     const params = {
       TableName: "riabatat-dev-users",
@@ -35,10 +35,15 @@ module.exports.handler = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+      'Access-Control-Allow-Origin': 'http://riabatat-dev-settings-page.s3-website.eu-central-1.amazonaws.com',
+      'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, PUT, DELETE',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
       body: JSON.stringify(
         {
           message: 'Settings saved successfully',
-          updateData: updateData,
+          updateData: unmarshall(updateData.Attributes),
         },
         null,
         2
